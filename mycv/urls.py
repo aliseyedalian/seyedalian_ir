f1cd654238863b619bd8ca497e5353b1699f4286
sedaliSedalian <seyedalian@outlook.com>
2022-05-03 18:31:04 +0430

    template is almost ready

--- mycv/urls.py
+++ mycv/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home, name='home') 
+    path('',views.home, name='home'),
+    path('about.html',views.about,name='about'),
+    path('blogs.html',views.blogs,name='blogs'),
+    path('resume.pdf',views.resume,name='resume'),
 ]
--- mycv/views.py
+++ mycv/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from django.http import FileResponse
+import os
 
 # Create your views here.
 def home(request):
 	return render(request,'index.html',{})
+
+def about(request):
+	return render(request,'about.html',{})
+
+def blogs(request):
+	return render(request,'blogs.html',{})
+
+def resume(request):
+	filepath = os.path.join('static', 'resume/resume.pdf')
+	return FileResponse(open(filepath, 'rb'), content_type='application/pdf')
