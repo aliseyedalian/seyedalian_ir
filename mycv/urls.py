50197f31ef93274ff0ec1af82aa0f65d9174c6d8
sedaliSedalian <seyedalian@outlook.com>
2022-05-04 19:57:47 +0430

    v1

--- mycv/urls.py
+++ mycv/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name='home'),
-    path('about.html',views.about,name='about'),
     path('blogs.html',views.blogs,name='blogs'),
     path('resume.pdf',views.resume,name='resume'),
 ]
--- mycv/views.py
+++ mycv/views.py
@@ -6,9 +6,6 @@ import os
 def home(request):
 	return render(request,'index.html',{})
 
-def about(request):
-	return render(request,'about.html',{})
-
 def blogs(request):
 	return render(request,'blogs.html',{})
 
