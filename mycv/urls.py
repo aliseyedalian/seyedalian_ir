5a44f3bee9146cf5dc8853037cdce1fcb6a7e949
sedaliSedalian <seyedalian@outlook.com>
2022-05-05 22:04:10 +0430

    resume link updated

--- mycv/urls.py
+++ mycv/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('',views.home, name='home'),
     path('blogs.html',views.blogs,name='blogs'),
-    path('resume.pdf',views.resume,name='resume'),
 ]
--- mycv/views.py
+++ mycv/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.http import FileResponse
-import os
 
 # Create your views here.
 def home(request):
@@ -9,6 +7,3 @@ def home(request):
 def blogs(request):
 	return render(request,'blogs.html',{})
 
-def resume(request):
-	filepath = os.path.join('static', 'resume/resume.pdf')
-	return FileResponse(open(filepath, 'rb'), content_type='application/pdf')
